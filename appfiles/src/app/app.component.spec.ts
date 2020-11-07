020b6f3cc4af6c07aa0803240018a7b9c70fe4a5
Cerulean Canvas <42316559+cerulean-canvas@users.noreply.github.com>
2020-11-08 00:54:13 +0530

    Update app.component.spec.ts

--- appfiles/src/app/app.component.spec.ts
+++ appfiles/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'WebApp'`, () => {
+  it(`should have as title 'Docker'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('WebApp');
+    expect(app.title).toEqual('Docker');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to WebApp!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Docker!');
   });
 });
